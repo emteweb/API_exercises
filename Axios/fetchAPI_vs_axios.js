1c7d13bddca969284e43f6236a12c4cfd9d351d0
Marcin T <fake_mail@gmail.com>
2020-12-31 13:48:59 +0100

    axios and query string

--- Axios/fetchAPI_vs_axios.js
+++ Axios/fetchAPI_vs_axios.js
@@ -59,12 +59,19 @@ const addJoke = async () => {
     const newLI = document.createElement('LI');
     newLI.append(jokeText);
     jokes.append(newLI);
-} 
+}
 
-const getDadJoke = async ()=>{
-    const config = {headers: {Accept: 'application/json'}}
-    const res = await axios.get('https://icanhazdadjoke.com/', config);
-    return (res.data.joke);
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' } }
+        const res = await axios.get('https://icanhazdadjoke.com/', config);
+        return (res.data.joke);
+
+    }
+    catch (e) {
+        return "No jokes available"
+    }
+
 }
 
 button.addEventListener('click', addJoke)
